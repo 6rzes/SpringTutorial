e554929c00b759de5f302aaa7328db1ce22b35f9
Grzegorz Ullmann <gullmann@gmail.com>
2017-04-16 12:48:51 +0200

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,11 @@
+import service.CustomerService;
+import service.CustomerServiceImpl;
+
+public class Application {
+
+    public static void main(String[] args) {
+        CustomerService customerService = new CustomerServiceImpl();
+
+        System.out.println(customerService.findAll().get(0).getFirstName());
+    }
+}
---
+++ src/main/java/repository/HibernateCustomerRepositoryImpl.java
@@ -0,0 +1,23 @@
+package repository;
+
+import model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+
+    @Override
+    public List<Customer> findAll() {
+
+        List<Customer> customers = new ArrayList<>();
+
+        Customer customer = new Customer();
+        customer.setFirstName("Jan");
+        customer.setLastName("Kowalski");
+        customers.add(customer);
+
+        return customers;
+    }
+
+}
